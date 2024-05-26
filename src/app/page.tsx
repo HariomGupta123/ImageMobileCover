932df2da4fe1c4bd753dde5bbce9a75cff58d548
Hariom Gupta <hg7716505@gmail.com>
2024-05-26 17:45:44 +0545

    added maxWidthwrapper component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 import Image from "next/image";
-
 export default function Home() {
   return (
     <div className="bg-slate-50">
@@ -8,6 +7,13 @@ export default function Home() {
         <MaxWidthWrapper className="pb-24 bt-10 lg:grid lg:grid-cols-3 sm:pb-32 lg:gap-x-0 xl:gap-x-8 lg:bt-24 xl:pt-32 lg:pb-52">
           <div className="col-span-2 px-6 lg:px-0 lg:pt-6 ">
             <div className=" mx-auto relative text-center lg:text-left flex flex-col items-center lg:items-start">
+              <div className="absolute w-28 left-0 -top-20 hidden lg:block  ">
+                <image src='/snake-1.png' className="w-full"/>
+              </div>
+              <h1 className="relateive w-fit tracking-tight text-balance mt-16 font-bold !leading-tight text-gray-900 text-5xl md:text-6xl lg:text-7xl ">your Image on a <span className="bg-green-600 px-2 text-white">custom</span>Phone case</h1>
+              <p className="mt-8 text-lg lg:pr-10 max-w-prose text-center lg:text-left text-balance md:text-wrap "> capture your favorite memories with your own,<span className="font-semibold">one-of-one</span>
+              phone case.
+              CaseCobra allows you to protect your memories ,not just your phone case</p>
             </div>
           </div>
         </MaxWidthWrapper>
