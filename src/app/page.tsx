cecfde7d3fe744ffed51f9fdb765381901df17c9
Hariom Gupta <hg7716505@gmail.com>
2024-05-27 16:05:36 +0545

    added NavBar component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 import { Check, Star } from "lucide-react";
 import snake1 from "../../public/snake-1.png"
@@ -69,7 +70,7 @@ export default function Home() {
           </div>
           <div className="col-span-full lg:col-span-1 w-full flex justify-center px-8 sm:px-16 md:px-0 mt-32 lg:mx-0 lg:mt-20 h-fit ">
             <div className=" relative md:max-w-xl">
-              <Image src={yourImage} className="absolute w-40 lg:w-52 left-56 -top-20 select-none hide sm:block lg:hidden xl:block" alt="/" />
+              <Image src={yourImage} className="absolute w-40 lg:w-52 left-56 -top-20 select-none hidden sm:block lg:hidden xl:block" alt="/" />
               <Image src={LineImage} alt="" className="absolute w-20 -left-6 -bottom-6 select-none" />
              <Phone className="w-64" imgSrc={testimonials1} />
             </div>
