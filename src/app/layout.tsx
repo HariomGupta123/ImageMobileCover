af30f37b6228e17229090004a081db6248c27936
Hariom Gupta <hg7716505@gmail.com>
2024-05-31 12:02:41 +0545

    install uploading library for store uploaded image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,10 +19,15 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <NavBar/>
-        {children}
-        <Footer/>
-        </body>
+        <NavBar />
+        <main className="flex flex-col min-h[calc(100vh-3.5rem-1px)]">
+          <div className="flex-1 flex flex-col h-full">
+            {children}
+          </div>
+          <Footer />
+        </main>
+
+      </body>
     </html>
   );
 }
