9ed4cc8fd779cd7b8822ee7a0eea7959d23fb880
Hariom Gupta <hg7716505@gmail.com>
2024-06-01 21:33:03 +0545

    added steps compenent and functionality

--- src/app/configure/layout.tsx
+++ src/app/configure/layout.tsx
@@ -3,8 +3,8 @@ import { ReactNode } from "react";
 
 const Layout = ({ children }: { children: ReactNode }) => {
     return (
-        <MaxWidthWrapper className="flex-1 flex flex-col">
-            {children}
-        </MaxWidthWrapper>)
+    <MaxWidthWrapper className="flex-1 flex flex-col">
+        {children}
+    </MaxWidthWrapper>)
 }
 export default Layout
---
+++ src/components/Steps.tsx
@@ -0,0 +1,52 @@
+"use client"
+import { cn } from '@/lib/utils';
+import { usePathname } from 'next/navigation'
+import path from 'path';
+import React from 'react'
+const STEPS = [
+  {
+    name: 'Step 1: Add image',
+    description: 'Choose an image for your case',
+    url: '/upload',
+  },
+  {
+    name: 'Step 2: Customize design',
+    description: 'Make the case yours',
+    url: '/design',
+  },
+  {
+    name: 'Step 3: Summary',
+    description: 'Review your final design',
+    url: '/preview',
+  },
+]
+export default function Steps() {
+    const pathname=usePathname();
+  return (
+    <ol className='rounded-md bg-white lg:flex lg:rounded-none lg:border-l lg:border-r lg:border-gray-200'>
+        {STEPS.map((step,i)=>{
+            const isCurrent=pathname.endsWith(step.url);
+            const isCompleted=STEPS.slice(i +1).some((step)=> pathname.endsWith(step.url))
+            const imgPath=`snake-${i +1}.png`
+            return(
+                <li key={step.name} className='relative overflow-hidden lg:flex-1'>
+                    <div>
+                        <span  className={cn(
+                  'absolute left-0 top-0 h-full w-1 bg-zinc-400 lg:bottom-0 lg:top-auto lg:h-1 lg:w-full',
+                  {
+                    'bg-zinc-700': isCurrent,
+                    'bg-primary': isCompleted,
+                  }
+                )}
+                aria-hidden='true'>
+
+                        </span>
+
+                    </div>
+                </li>
+            )
+        })}
+         
+    </ol>
+  )
+}
