f869b21b3bd7001cb8dc49442499cd5ee02a4d2e
Hariom Gupta <hg7716505@gmail.com>
2024-06-05 14:21:15 +0545

    completed option validator,

--- src/app/configure/design/DesignConfigurator.tsx
+++ src/app/configure/design/DesignConfigurator.tsx
@@ -1,7 +1,14 @@
+"use client"
+import HandleComponent from "@/components/HandleComponent";
 import { AspectRatio } from "@/components/ui/aspect-ratio";
-import phonetemplete from "../../../../public/phone-template.png"
-import Image from "next/image";
-
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { cn } from "@/lib/utils";
+import { COLORS } from "@/validators/option-validator";
+import NextImage from "next/image";
+import { useState } from "react";
+import { Rnd } from "react-rnd";
+import { RadioGroup} from "@headlessui/react"
+import { Label } from "@/components/ui/label";
 interface DesignConfiguratorProps {
     configId: string
     imageUrl: string
@@ -12,16 +19,72 @@ const DesignConfigurator = ({
     imageUrl,
     imageDimensions,
 }: { DesignConfiguratorProps }) => {
+    const [options,setOptions]=useState<{color:(typeof COLORS)[number]}>({ color:COLORS[0],})
     return (
         <div className='relative mt-20 grid grid-cols-1 lg:grid-cols-3 mb-20 pb-20'>
             <div className='relative h-[37.5rem] overflow-hidden col-span-2 w-full max-w-4xl flex items-center justify-center rounded-lg border-2 border-dashed border-gray-300 p-12 text-center focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2'>
                 <div className='relative w-60 bg-opacity-50 pointer-events-none aspect-[896/1831]'>
                     <AspectRatio ratio={896 / 1831} className=" pointer-events-none relative z-50 aspect-[896/1831] w-full" >
-                        <Image src={phonetemplete} alt="phone image" width={imageDimensions.width} height={imageDimensions.height} className=" pointer-events-none z-50 select-none" />
-
+                        <NextImage src='/phone-template.png' alt="phone image" width={imageDimensions.width} height={imageDimensions.height} className=" pointer-events-none z-50 select-none" />
                     </AspectRatio>
+                    <div className='absolute z-40 inset-0 left-[3px] top-px right-[3px] bottom-px rounded-[32px] shadow-[0_0_0_99999px_rgba(229,231,235,0.6)]' />
+                    <div
+                        className={cn(
+                            'absolute inset-0 left-[3px] top-px right-[3px] bottom-px rounded-[32px]',
+                            `bg-blue-950`
+                        )}
+                    />
                 </div>
-
+                <Rnd default={{
+                    x: 150,
+                    y: 205,
+                    height: imageDimensions.height / 4,
+                    width: imageDimensions / 4
+                }}
+                    lockAspectRatio
+                    resizeHandleComponent={{
+                        bottomRight: <HandleComponent />,
+                        bottomLeft: <HandleComponent />,
+                        topRight: <HandleComponent />,
+                        topLeft: <HandleComponent />,
+                    }}
+                    className='absolute z-20 border-[3px] border-primary'
+                >
+                    <div className='relative w-full h-full'>
+                        <NextImage
+                            src={imageUrl}
+                            fill
+                            alt='your image'
+                            className='pointer-events-none'
+                        />
+                    </div>
+                </Rnd>
+            </div>
+            <div className='h-[37.5rem] w-full col-span-full lg:col-span-1 flex flex-col bg-white'>
+                <ScrollArea className='relative flex-1 overflow-auto'>
+                    <div
+                        aria-hidden='true'
+                        className='absolute z-10 inset-x-0 bottom-0 h-12 bg-gradient-to-t from-white pointer-events-none'
+                    />
+                    <div className='px-8 pb-12 pt-8'>
+                        <h2 className='tracking-tight font-bold text-3xl'>
+                            Customize your case
+                        </h2>
+                           <div className='w-full h-px bg-zinc-200 my-6' />
+                           <div className='relative mt-4 h-full flex flex-col justify-between'>
+                             <div className='flex flex-col gap-6'>
+                                 <RadioGroup value={options.color} 
+                                 onChange={(val)=>{
+                                    setOptions((prev)=>({ ...prev,color:val}))
+                                 }} >
+                                    <Label>
+                                        Color:{options.color.label}
+                                    </Label>
+                                 </RadioGroup>
+                             </div>
+                           </div>
+                    </div>
+                </ScrollArea>
             </div>
         </div>
     )
