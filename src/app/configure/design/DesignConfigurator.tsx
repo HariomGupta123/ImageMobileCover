2b82e7e6f76d0396a430800dcac606459e55b8bc
Hariom Gupta <hg7716505@gmail.com>
2024-06-07 23:00:51 +0545

    connected cropped image to database

--- src/app/configure/design/DesignConfigurator.tsx
+++ src/app/configure/design/DesignConfigurator.tsx
@@ -2,16 +2,20 @@
 import HandleComponent from "@/components/HandleComponent";
 import { AspectRatio } from "@/components/ui/aspect-ratio";
 import { ScrollArea } from "@/components/ui/scroll-area";
-import { cn } from "@/lib/utils";
-import { COLORS, MATERIALS, MODELS } from "@/validators/option-validator";
+import { cn, formatPrice } from "@/lib/utils";
+import { COLORS, FINISHES, MATERIALS, MODELS } from "@/validators/option-validator";
 import NextImage from "next/image";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { Rnd } from "react-rnd";
 import { RadioGroup } from "@headlessui/react"
 import { Label } from "@/components/ui/label";
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
 import { Button } from "@/components/ui/button";
-import { Check, ChevronsUpDown } from "lucide-react";
+import { ArrowRight, Check, ChevronsUpDown } from "lucide-react";
+import { BASE_PRICE } from "@/config/products";
+import { resolve } from "path";
+import { useUploadThing } from "@/lib/uploadthing";
+import { useToast } from "@/components/ui/use-toast";
 interface DesignConfiguratorProps {
     configId: string
     imageUrl: string
@@ -22,19 +26,89 @@ const DesignConfigurator = ({
     imageUrl,
     imageDimensions,
 }: { DesignConfiguratorProps }) => {
+    const {toast}=useToast();
     const [options, setOptions] = useState<
         {
             color: (typeof COLORS)[number]
             model: (typeof MODELS.options)[number]
-            material:(typeof MATERIALS.options)[number]
+            material: (typeof MATERIALS.options)[number]
+            finish: (typeof FINISHES.options)[number]
 
-        }>({ color: COLORS[0], model: MODELS.options[0],material:MATERIALS.options[0] })
+        }>({ color: COLORS[0], model: MODELS.options[0], material: MATERIALS.options[0], finish: FINISHES.options[0] })
+
+        const [renderedDimension,setrenderDimension]=useState({
+            width:imageDimensions.width / 4,
+            height:imageDimensions.height /4,
+        })
+        const [renderedPosition,setrenderPosition]=useState({
+            x:150,
+           y:205
+        })
+        const phoneCaseRef=useRef<HTMLDivElement>(null);
+        const containerCaseRef=useRef<HTMLDivElement>(null)
+        const {startUpload}=useUploadThing('imageUploader')
+        async function saveConfiguration(){
+            try{
+                const {left:leftcase,top:topcase,width,height}=phoneCaseRef.current!.getBoundingClientRect()
+                const {left:containerLeft,top:containerTop}=containerCaseRef.current!.getBoundingClientRect()
+                const leftOffset=leftcase - containerLeft
+                const topOffset=topcase - containerTop
+                const actualX=renderedPosition.x -leftOffset
+                const actualY=renderedPosition.y -topOffset
+                const canvas=document.createElement('canvas');
+                canvas.width=width
+                canvas.height=height
+                const ctx =canvas.getContext('2d');
+                const userImage=new Image();
+                userImage.crossOrigin='anonymous'
+                userImage.src=imageUrl
+                await new Promise((resolve)=>(userImage.onload=resolve))
+                ctx?.drawImage(userImage,actualX,actualY,
+                    renderedDimension.width,
+                    renderedDimension.height
+
+                )
+                const base64=canvas.toDataURL();
+              
+                const base64Date=base64.split(',')[1];
+               
+                const blob = base64ToBlob(base64Date,"image/png");
+                  console.log(blob)
+                const file=new File([blob],'filename.png',{type:'image/png'})
+                     console.log(file)
+                startUpload([file],{configId})
+            } catch(err){
+                toast({
+                    title:'something went wrong',
+                    description:'There was a problem saving your config ,please try again',
+                    variant:'descriptive',
+                })
+
+            }
+            function base64ToBlob(base64:string,mimeType:string){
+                const byteCharacters=atob(base64);
+                  
+                const byteNumbers=new Array(byteCharacters.length);
+              
+                for (let i = 0; i< byteCharacters.length; i++) {  
+
+                        byteNumbers[i] =byteCharacters.charCodeAt(i)
+                     
+                        //charCodeAt() is used for unicode of specified character 
+               
+                }
+                const byteArray =new Uint8Array(byteNumbers);
+                return new Blob([byteArray],{type:mimeType})
+
+            }
+        }
+
     return (
         <div className='relative mt-20 grid grid-cols-1 lg:grid-cols-3 mb-20 pb-20'>
-            <div className='relative h-[37.5rem] overflow-hidden col-span-2 w-full max-w-4xl flex items-center justify-center rounded-lg border-2 border-dashed border-gray-300 p-12 text-center focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2'>
+            <div ref={containerCaseRef} className='relative h-[37.5rem] overflow-hidden col-span-2 w-full max-w-4xl flex items-center justify-center rounded-lg border-2 border-dashed border-gray-300 p-12 text-center focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2'>
                 <div className='relative w-60 bg-opacity-50 pointer-events-none aspect-[896/1831]'>
-                    <AspectRatio ratio={896 / 1831} className=" pointer-events-none relative z-50 aspect-[896/1831] w-full" >
-                        <NextImage src='/phone-template.png' alt="phone image" width={imageDimensions.width} height={imageDimensions.height} className=" pointer-events-none z-50 select-none" />
+                    <AspectRatio ratio={896 / 1831} ref={phoneCaseRef} className=" pointer-events-none relative z-50 aspect-[896/1831] w-full" >
+                        <NextImage src='/phone-template.png' fill alt="phone image" className=" pointer-events-none z-50 select-none" />
                     </AspectRatio>
                     <div className='absolute z-40 inset-0 left-[3px] top-px right-[3px] bottom-px rounded-[32px] shadow-[0_0_0_99999px_rgba(229,231,235,0.6)]' />
                     <div
@@ -47,8 +121,20 @@ const DesignConfigurator = ({
                 <Rnd default={{
                     x: 150,
                     y: 205,
-                    height: imageDimensions.height / 4,
-                    width: imageDimensions / 4
+                    height: imageDimensions.height /4,
+                    width:imageDimensions.width / 4
+                }}
+                onResizeStop={(_,__,ref,___,{x,y})=>{
+                    setrenderDimension({
+                        height:parseInt(ref.style.height.slice(0,-2)),
+                        width:parseInt(ref.style.height.slice(0,-2)),
+                    })
+                    setrenderPosition({x,y})
+
+                }}
+                onDragStop={(_,data)=>{
+                    const {x,y}=data
+                    setrenderPosition({x,y})
                 }}
                     lockAspectRatio
                     resizeHandleComponent={{
@@ -62,7 +148,8 @@ const DesignConfigurator = ({
                     <div className='relative w-full h-full'>
                         <NextImage
                             src={imageUrl}
-                            fill
+                         fill
+                      
                             alt='your image'
                             className='pointer-events-none'
                         />
@@ -149,11 +236,74 @@ const DesignConfigurator = ({
                                     </DropdownMenu>
 
                                 </div>
+                                {[MATERIALS, FINISHES].map(({ name, options: selectableOptions }) => (
+                                    <RadioGroup key={name} value={options[name]} onChange={(val) =>
+                                        setOptions((prev) => (
+                                            {
+                                                ...prev,
+                                                //name:material /finish
+                                                [name]: val,
+                                            }
+
+                                        ))
+                                    }>
+                                        <Label>
+                                            {name.slice(0, 1).toUpperCase() + name.slice(1)}
+                                        </Label>
+                                        <div className='mt-3 space-y-4' >
+                                            {selectableOptions.map((option) => (
+                                                <RadioGroup.Option key={option.value} value={option} className={({ active, checked }) =>
+                                                    cn(
+                                                        'relative block cursor-pointer rounded-lg bg-white px-6 py-4 shadow-sm border-2 border-zinc-200 focus:outline-none ring-0 focus:ring-0 outline-none sm:flex sm:justify-between',
+                                                        {
+                                                            'border-primary': active || checked,
+                                                        }
+                                                    )
+                                                } >
+                                                    <span className='flex items-center'>
+                                                        <span className='flex flex-col text-sm'>
+                                                            <RadioGroup.Label
+                                                                className='font-medium text-gray-900'
+                                                                as='span'>
+                                                                {option.label}
+                                                            </RadioGroup.Label>
+                                                            {option.description ? (<RadioGroup.Description as='span' className='text-gray-500' >
+                                                                <span className='block sm:inline' >{option.description}</span>
+                                                            </RadioGroup.Description>) : null}
+                                                        </span>
+                                                    </span>
+                                                    <RadioGroup.Description as='span'
+                                                        className='mt-2 flex text-sm sm:ml-4 sm:mt-0 sm:flex-col sm:text-right'>
+                                                        <span className='font-medium text-gray-900'>
+                                                            {formatPrice(option.price / 100)}
+                                                        </span>
+
+                                                    </RadioGroup.Description>
+
+                                                </RadioGroup.Option>
+                                            ))}
+
+                                        </div>
+
+                                    </RadioGroup>
+                                ))}
                             </div>
                         </div>
                     </div>
                 </ScrollArea>
+                <div className='w-full px-8 h-16 bg-white'>
+                    <div className='h-px w-full bg-zinc-200' />
+                    <div className='w-full h-full flex justify-end items-center'>
+                        <div className='w-full flex gap-6 items-center'>
+                            <p className='font-medium whitespace-nowrap'>
+                                {formatPrice((BASE_PRICE + options.finish.price + options.material.price)/100)}
+                            </p>
+                            <Button  size="sm" onClick={()=>saveConfiguration()} > Contineu <ArrowRight className="h-4 w-4 ml-1.5 inline"/></Button>
+                        </div>
+                    </div>
+                </div>
             </div>
+
         </div>
     )
 }
