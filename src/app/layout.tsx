330731150f8ee407ec5298c06fb25274f7750f16
HariomGupta123 <hg7716505@gmail.com>
2024-10-21 16:24:01 +0545

    given right path to navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,11 @@
 import type { Metadata } from 'next'
 import { Recursive } from 'next/font/google'
 import './globals.css'
-import Navbar from '@/components/Navbar'
 import Footer from '@/components/Footer'
 import { Toaster } from '@/components/ui/toaster'
 import Providers from '@/components/Providers'
 import { constructMetadata } from '@/lib/utils'
+import NavBar from '@/components/Navbar'
 
 const recursive = Recursive({ subsets: ['latin'] })
 
@@ -19,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body className={recursive.className}>
-        <Navbar />
+        <NavBar/>
 
         <main className='flex grainy-light flex-col min-h-[calc(100vh-3.5rem-1px)]'>
           <div className='flex-1 flex flex-col h-full'>
